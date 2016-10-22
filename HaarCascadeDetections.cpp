1877744e1103552e50a3215b1a235aec03314114
root <root@BURSTLINKER2.home>
2016-10-21 23:47:02 -0400

    first commit

---
+++ HaarCascadeDetections.cpp
@@ -0,0 +1,33 @@
+#include<string>
+#include <opencv2/core/core.hpp>
+#include <opencv2\highgui.hpp>
+#include <opencv2\opencv.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include"HaarCascadeDetections.h"
+
+using namespace std;
+
+namespace HaarDetections {
+
+	void DetectFace(string cascadeClassifierFile) 
+	{
+		if (cascadeClassifierFile.empty())
+			return;
+		else
+		{
+		
+		}
+	}
+	void DetectEyes(string cascadeClassifierFile)
+	{
+
+	}
+
+
+
+
+
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2\highgui.hpp>
+#include <opencv2\opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+
+//short program which shows how to convert an image into greyscale
+int main()
+{
+	const string faceCascade = "C:\OpenCV - 3.1.0\opencv\build\etc\haarcascades";
+
+	//set up camera
+	VideoCapture capture(0);
+	if (!capture.isOpened())
+		cout << "No camera or problem with camera" << endl;
+
+	//window to display camera capture
+	namedWindow("videoCapture", 1);
+	namedWindow("blur", 1);
+	//main loop
+	for (;;) 
+	{
+		//Get new frame from buffer
+		Mat frame;
+		capture >> frame;
+		Mat blurred;
+		//some smoothing
+		GaussianBlur(frame, blurred, Size(9, 9), 1.5, 1.5);
+		imshow("videoCapture", frame);
+		imshow("blur", blurred);
+		if (waitKey(30) >= 0) break;
+
+	}
+
+}
